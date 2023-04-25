f3113973c5fa4f7c8e2c2de69b40e01c791a6e79
Yulia Gorbaneva <yarantseva89@bk.ru>
2023-04-25 11:39:28 +0600

    lessen3

---
+++ Exs_1/Program.cs
@@ -0,0 +1,48 @@
+﻿void Metod1()
+{
+    Console.WriteLine("User name");
+}
+
+Metod1();
+
+void Metod2(string msg)
+{
+    Console.WriteLine(msg);   
+}
+
+Metod2(msg: "Text messeng");
+
+void Metod_21 (string msg, int count);
+
+int i =0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i ++;
+    }
+Metod_21(msg:"Text", count: 4);
+
+
+int Metod3()
+{
+    return DateTime.Now.Year;
+}
+
+int yaer = Metod3();
+Console.WriteLine(yaer);
+
+string Metod4(int count, string c);
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while(i < count)
+    {
+        result= result + c;
+        i ++;
+    }
+    return result;
+}
+string res = Metod4(10, "Text");
+Console.WriteLine(res);
+
---
+++ Exs_2/Program.cs
@@ -0,0 +1,15 @@
+﻿string Metod5(int count, string c);
+{
+    
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++);
+    {
+        result = result + c;
+    }
+    return result;
+}
+string res = Metod5(5, "Text");
+Console.WriteLine(res);
+
+
