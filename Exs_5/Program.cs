1ca9b473212a585dc16ea8ef213ea8796de9a3a8
Yulia Gorbaneva <yarantseva89@bk.ru>
2023-04-25 16:43:59 +0600

    lassen3-add2

--- Exs_5/Program.cs
+++ Exs_5/Program.cs
@@ -1,4 +1,36 @@
 ﻿//упорядочить массив
 //алгоритм сортировки методом выбора(Min, Max)
 
+int [] arr = {1, 7, 8, 6, 9, 5, 2, 4, 3, 6, 9};
 
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+     Console.Write($"{array[i]} ");   
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp; 
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
